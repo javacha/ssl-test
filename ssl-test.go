3db24c0456f21125c3afb5c7dcabaa2ca5117045
nombre <email>
2024-07-09 11:50:10 -0300

    Mejora en chequeo de errores y mensajes

--- ssl-test.go
+++ ssl-test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"crypto/tls"
 	"crypto/x509"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -11,7 +12,7 @@ import (
 
 func help() {
 	fmt.Print("no args!  Must inform https://<server> to test. \n\n\n")
-	fmt.Print("Put server CAs in file myTrustStore.pem. \n\n")
+	fmt.Print("Put server CAs into myTrustStore.pem file if needed. \n\n")
 	return
 }
 
@@ -29,13 +30,22 @@ func main() {
 	certFile := "myTrustStore.pem"
 	cert, err := ioutil.ReadFile(certFile)
 	if err != nil {
-		fmt.Println("Error reading certificate:", err)
-		return
+		if !errors.Is(err, os.ErrNotExist) {
+			fmt.Println("Error reading file:", err)
+			return
+		}
 	}
 
-	// Create a certificate pool and add your server's certificate
 	caCertPool := x509.NewCertPool()
-	caCertPool.AppendCertsFromPEM(cert)
+	if len(cert) > 0 {
+		// Create a certificate pool and add your server's certificate
+		caCertPool.AppendCertsFromPEM(cert)
+		fmt.Println(">>> Using custom truststore ", certFile)
+	} else {
+		fmt.Println(">>> Using system CAs")
+		caCertPool = nil
+	}
+	fmt.Println("")
 
 	// Create a custom HTTP client with your certificate
 	client := &http.Client{
@@ -46,22 +56,26 @@ func main() {
 		},
 	}
 
+	connectOK := true
+
 	// Make an HTTPS request (replace with your API URL)
 	resp, err := client.Get(url)
 	if err != nil {
-		fmt.Println("Error sending request:", err)
-		return
-	}
-	defer resp.Body.Close()
-
-	fmt.Print("\nConnected  OK!\n\n")
-
-	// Read the response body
-	body, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		fmt.Println("Error reading response:", err)
-		return
+		connectOK = false
 	}
 
-	fmt.Printf("Response from %s:\n%s\n", url, body)
+	if connectOK {
+		defer resp.Body.Close()
+
+		fmt.Print("\n<<< Connected  OK!\n\n")
+		body, err := ioutil.ReadAll(resp.Body) // Read the response body
+		if err != nil {
+			fmt.Println("Error reading response:", err)
+			return
+		}
+		fmt.Printf("Response from %s:\n%s\n%s\n", url, resp.Status, body)
+	} else {
+		fmt.Print("\n<<< Error connecting!\n\n")
+		fmt.Printf("Response from %s:\n%s\n", url, err)
+	}
 }
